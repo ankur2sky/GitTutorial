886e82d924c32a17a66990d18ed0b85c30af068b
Ankur <40185220+ankur2sky@users.noreply.github.com>
2025-06-03 11:05:50 +0100

    Create ClassicPage.java

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,14 @@
+public class ClassicPage {
+
+	
+	public void classic() {
+		
+		int n = 100;
+		int m = 200;
+		int div = m/n;
+    System.out.println(div);
+    System.out.println("bye");
+		
+	}
+
+}
